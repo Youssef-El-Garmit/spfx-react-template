773e0790ff36d0a3bee5ef48c11f1907ef69835e
Youssef <youssef.elgarmit@hotmail.com>
2025-08-21 02:53:43 +0200

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "webpart/src/webparts/assets",
-    assetsDir: "appcode"
-  }
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 })
